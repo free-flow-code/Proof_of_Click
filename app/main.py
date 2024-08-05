43de3be04e3b128f0c90b176ceab5178a1999941
free-flow-code <126948647+free-flow-code@users.noreply.github.com>
2024-08-06 01:31:44 +0300

    Delete static, pages and templates from backend.

--- app/main.py
+++ app/main.py
@@ -1,19 +1,17 @@
 import logging
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
 from contextlib import asynccontextmanager
 
 from app.config import settings
-from app.redis_init import redis_client, init_redis
+from app.redis_init import init_redis
 from app.game_items.router import router as items_router
 from app.users.router import router as users_router
 from app.improvements.router import router as improvements_router
 from app.lots.router import router as lots_router
 from app.notifications.router import router as notification_router
-from app.pages.router import router as pages_router
 from app.clicks.router import router as clicks_router
 
 logging.basicConfig(
@@ -38,14 +36,11 @@ app = FastAPI(
     lifespan=lifespan,
     )
 
-app.mount("/static", StaticFiles(directory="app/static"), "static")
-
 app.include_router(users_router)
 app.include_router(items_router)
 app.include_router(improvements_router)
 app.include_router(lots_router)
 app.include_router(notification_router)
-app.include_router(pages_router)
 app.include_router(clicks_router)
 
 origins = settings.ORIGINS
