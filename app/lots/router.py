c8d399d6e94308fe10064337abbb2b8a2830ad03
free-flow-code <126948647+free-flow-code@users.noreply.github.com>
2024-11-15 00:49:29 +0300

    Added docstrings for lots endpoint.

--- app/lots/router.py
+++ app/lots/router.py
@@ -15,6 +15,15 @@ router = APIRouter(
 
 @router.get("")
 async def get_user_lots(current_user=Depends(get_current_user)) -> list[SLots]:
+    """
+    Возвращает список лотов на продажу игровых предметов текущего пользователя.
+
+    Args:
+        current_user: Текущий пользователь.
+
+    Returns:
+        list[SLots]: Список лотов.
+    """
     return await LotsDAO.find_by_user_id(int(current_user["id"]))
 
 
@@ -29,6 +38,25 @@ async def add_lot(
         best_price_user_id: Optional[int],
         current_user=Depends(get_current_user)
 ):
+    """
+    Добавляет новый лот для пользователя. Доступно только для администратора.
+
+    Args:
+        user_id (int): Идентификатор пользователя, для которого добавляется лот.
+        date_at_create (date): Дата создания лота.
+        expiration_date (date): Дата закрытия лота.
+        game_item_id (int): Идентификатор игрового предмета, который выставлен на продажу.
+        start_price (float): Стартовая цена лота.
+        best_price (float): Лучшая цена, предложенная другим пользователем.
+        best_price_user_id (float): Идентификатор пользователя, давшего лучшую цену.
+        current_user: Текущий пользователь.
+
+    Returns:
+        dict: Содержит сообщение о результате добавления лота.
+
+    Raises:
+        AccessDeniedException: Если текущий пользователь не имеет прав администратора.
+    """
     if current_user["role"] != "admin":
         raise AccessDeniedException
 
@@ -46,6 +74,20 @@ async def add_lot(
 
 @router.delete("/{lot_id}")
 async def delete_lot(lot_id: int, current_user=Depends(get_current_user)):
+    """
+    Удаляет лот по его идентификатору. Доступно только для администратора.
+
+    Args:
+        lot_id (int): Идентификатор удаляемого лота.
+        current_user: Текущий пользователь.
+
+    Returns:
+        dict: Содержит сообщение о результате удаления лота.
+
+    Raises:
+        AccessDeniedException: Если текущий пользователь не имеет прав администратора.
+        ObjectNotFoundException: Если лот с указанным идентификатором не найден.
+    """
     if current_user["role"] != "admin":
         raise AccessDeniedException
 
